505c3849479f610f49e1248228e7e830df91ffb2
Jeff Pollard <jeff.pollard@gmail.com>
2012-06-16 13:20:15 -0700

    Comform to Go best practices

--- newton.go
+++ newton.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-  "fmt"
-  "math"
+	"fmt"
+	"math"
 )
 
 const Precision = 1e-15
 
 func Newton(guess, input float64) float64 {
-  return guess - (guess*guess-input)/(2*guess)
+	return guess - (guess*guess-input)/(2*guess)
 }
 
 func Sqrt(input float64) float64 {
-  var guess, delta float64 = input, 1
+	var guess, delta float64 = input, 1
 
-  for delta > Precision {
-    // fmt.Println(guess, delta)
-    old_guess := guess
+	for delta > Precision {
+		// fmt.Println(guess, delta)
+		old_guess := guess
 
-    guess = Newton(guess, input)
-    delta = math.Abs(guess - old_guess)
-  }
+		guess = Newton(guess, input)
+		delta = math.Abs(guess - old_guess)
+	}
 
-  return guess
+	return guess
 }
 
 func main() {
-    fmt.Println(9, Sqrt(9))
-    fmt.Println(25, Sqrt(25))
-    fmt.Println(100, Sqrt(100))
-    fmt.Println(52, Sqrt(52))
+	fmt.Println(9, Sqrt(9))
+	fmt.Println(25, Sqrt(25))
+	fmt.Println(100, Sqrt(100))
+	fmt.Println(52, Sqrt(52))
 }
--- pic.go
+++ pic.go
@@ -4,24 +4,25 @@ import "code.google.com/p/go-tour/pic"
 import "math/rand"
 
 func Pic(x, y int) [][]uint8 {
-  picture := make([][]uint8, y)
+	picture := make([][]uint8, y)
 
-  for row := range picture {
-    picture[row] = make([]uint8, x)
+	for row := range picture {
+		picture[row] = make([]uint8, x)
 
-    for col := range picture[row] {
-      // picture[row][col] = uint8(row^col + (row + col) / 2)
-      // picture[row][col] = uint8(row ^ col + row * math.Pi)
-      // picture[row][col] = uint8(row * col)
-      picture[row][col] = uint8(rand.Intn(255))
-      // picture[row][col] = uint8((row + col) / 2)
-      // picture[row][col] = uint8(row^2 + col)
-    }
-  }
+		for col := range picture[row] {
+			// picture[row][col] = uint8(row^col + (row + col) / 2)
+			// picture[row][col] = uint8(row ^ col + row * math.Pi)
+			// picture[row][col] = uint8(row * col)
+			picture[row][col] = uint8(rand.Intn(255))
+			rand.Int()
+			// picture[row][col] = uint8((row + col) / 2)
+			// picture[row][col] = uint8(row^2 + col)
+		}
+	}
 
-  return picture
+	return picture
 }
 
 func main() {
-  pic.Show(Pic)
+	pic.Show(Pic)
 }
